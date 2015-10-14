c93c24564ca3607003cfb26413dcad87f20852a0
Benoit Tremblay <trembl.ben@gmail.com>
2015-10-14 09:46:04 -0400

    Release 0.1.6

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'webpack:webpack',
-    version: '0.1.5',
+    version: '0.1.6',
     summary: 'Seamlessly integrate Webpack with Meteor build system',
     git: 'https://github.com/thereactivestack/meteor-webpack.git',
     documentation: 'README.md'
