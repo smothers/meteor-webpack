fe3af899a423d9488ac5977b3a98fd5592f01605
Benoit Tremblay <trembl.ben@gmail.com>
2015-12-29 11:31:54 -0500

    Update npmworkaround package version

--- packages/npmworkaround/package.js
+++ packages/npmworkaround/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'webpack:npmworkaround',
-    version: '0.1.0',
+    version: '1.0.0',
     summary: 'Internal package for webpack',
     git: 'https://github.com/thereactivestack/meteor-webpack.git',
     documentation: 'README.md'
