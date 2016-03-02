5385aef053afd27cdd7a8bbb67b06cf441a3c229
Benoit Tremblay <trembl.ben@gmail.com>
2016-03-01 23:18:03 -0500

    Add public config to devServer

--- packages/reload/reload.js
+++ packages/reload/reload.js
@@ -20,7 +20,7 @@ function patchReload() {
   Meteor.startup(() => {
     // Use the script from the webpack dev server
     const target = Meteor.isCordova ? 'cordova' : 'web';
-    const devServerOrigin = __WebpackDevServerConfig__.protocol + '//' + __WebpackDevServerConfig__.host + ':' + __WebpackDevServerConfig__.port;
+    const devServerOrigin = __WebpackDevServerConfig__.protocol + '//' + (__WebpackDevServerConfig__.public || (__WebpackDevServerConfig__.host + ':' + __WebpackDevServerConfig__.port));
 
     const scriptEl = document.createElement('script');
     scriptEl.type = 'text/javascript';
