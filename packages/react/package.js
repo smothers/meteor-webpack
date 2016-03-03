0687b8e7a8e48406b491d4ed0d006814621d83f0
Benoit Tremblay <trembl.ben@gmail.com>
2016-03-02 22:43:40 -0500

    Released webpack:react 1.1.1

--- packages/react/package.js
+++ packages/react/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'webpack:react',
-    version: '1.1.0',
+    version: '1.1.1',
     summary: 'Integrate React with Webpack',
     git: 'https://github.com/thereactivestack/meteor-webpack.git',
     documentation: 'README.md'
