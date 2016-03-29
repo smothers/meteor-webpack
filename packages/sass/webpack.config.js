224ce11e8bd0ef6af108c196f85fb1cfae278169
Benoit Tremblay <trembl.ben@gmail.com>
2016-03-29 05:38:26 -0400

    Refactor sass plugin

--- packages/sass/webpack.config.js
+++ packages/sass/webpack.config.js
@@ -1,24 +1,28 @@
 var weight = 300;
 
 function dependencies(settings) {
+  const devDependencies = {
+    'sass-loader': '^3.1.2',
+    'node-sass': '^3.4.2'
+  };
+
+  if (settings.sassResources) {
+    devDependencies['sass-resources-loader'] = '^1.0.2';
+  }
+
   return {
-    devDependencies: {
-      'sass-loader': '^3.1.2',
-      'node-sass': '^3.4.2',
-      "sass-resources-loader": "1.0.2"
-    }
+    devDependencies: devDependencies
   };
 }
 
 function config(settings, require) {
   var plugins = [];
-  var cssLoader;
-  var config = settings.sassResources ? { "sassResources":settings.sassResources }  : {};
+  var config = {};
+  var cssLoader = settings.cssLoader + '!sass?' + JSON.stringify(settings.sass || {});
 
-  if(settings.sassResources){
-    cssLoader = settings.cssLoader + '!sass!sass-resources?' + JSON.stringify(settings.sass || {});
-  }else {
-    cssLoader = settings.cssLoader + '!sass?' + JSON.stringify(settings.sass || {});
+  if (settings.sassResources) {
+    config.sassResources = settings.sassResources;
+    cssLoader += '!sass-resources';
   }
 
   if (settings.cssExtract) {
@@ -29,6 +33,6 @@ function config(settings, require) {
   return {
     loaders: [{ test: /\.scss$/, loader: cssLoader }],
     extensions: ['.scss'],
-    config:config
+    config: config
   };
 }
