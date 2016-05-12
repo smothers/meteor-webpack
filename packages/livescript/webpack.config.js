c425b7506cbdf982377b93b6a6bfc08a94d888ec
Dmitry Novikov <glowinglens@gmail.com>
2016-05-12 21:52:25 +0300

    Add LiveScript package

---
+++ packages/livescript/webpack.config.js
@@ -0,0 +1,58 @@
+var weight = 200;
+
+function dependencies() {
+  return {
+    devDependencies: {
+      'babel': '^6.3.26',
+      'babel-core': '^6.3.26',
+      'babel-loader' : '^6.2.0',
+      'babel-preset-es2015': '^6.3.13',
+      'babel-preset-stage-0': '^6.3.13',
+      'livescript': '^1.4.0',
+      'livescript-loader': '^0.1.5'
+    }
+  };
+}
+
+function config(settings, require) {
+  var fs = require('fs');
+  var path = require('path');
+  var babelSettings = {};
+
+  var CWD = path.resolve('./');
+
+  if (fs.existsSync(CWD + '/.babelrc')) {
+    var babelrc = fs.readFileSync(CWD + '/.babelrc');
+    babelSettings = JSON.parse(babelrc);
+  }
+
+  if (!babelSettings.presets) {
+    babelSettings.presets = [];
+  }
+
+  if (!babelSettings.plugins) {
+    babelSettings.plugins = [];
+  }
+
+  if (babelSettings.presets.indexOf('es2015') < 0) {
+    babelSettings.presets.push('es2015');
+  }
+
+  if (babelSettings.presets.indexOf('stage-0') < 0 &&
+      babelSettings.presets.indexOf('stage-1') < 0 &&
+      babelSettings.presets.indexOf('stage-2') < 0 &&
+      babelSettings.presets.indexOf('stage-3') < 0) {
+    babelSettings.presets.push('stage-0');
+  }
+
+  if (settings.babel && settings.babel.plugins) {
+    babelSettings.plugins = babelSettings.plugins.concat(settings.babel.plugins);
+  }
+
+  return {
+    loaders: [
+      { test: /\.ls$/, loader: 'babel?' + JSON.stringify(babelSettings) + '!livescript', exclude: /\.meteor|node_modules/ }
+    ],
+    extensions: ['.ls'],
+  };
+}
